69e839332926202a4b1cb5f1f79dc185ddf4fbc4
Dhruv <dhruv18999@gmail.com>
2023-10-08 16:27:04 -0500

    -- working support action without attack temp working

--- Assets/Assignment1/Scripts/Characters.cs
+++ Assets/Assignment1/Scripts/Characters.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02f0f435bd6b772774bd45a03f3282ea739695fbc0f927c521214780f013a4c7
-size 1373
+oid sha256:e1a2e360fbcfce1fae5f0e44eccdb8dc3bbd14e43a796f5b0971434a3db1eba6
+size 1214
