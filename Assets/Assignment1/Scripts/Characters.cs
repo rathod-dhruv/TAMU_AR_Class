ddda9ce3cddbca0fcb4675b4e93fece6e0a9c032
Dhruv <dhruv18999@gmail.com>
2023-10-08 16:45:01 -0500

    ---

--- Assets/Assignment1/Scripts/Characters.cs
+++ Assets/Assignment1/Scripts/Characters.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1a2e360fbcfce1fae5f0e44eccdb8dc3bbd14e43a796f5b0971434a3db1eba6
-size 1214
+oid sha256:de2374bbf175c4601c89a55b711afe17ddd3ab7239fa629ca1ef818facb1176c
+size 1207
