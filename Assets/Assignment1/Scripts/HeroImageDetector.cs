464c0cb2f67658ea30cb40421c41754e96e5a763
Dhruv <dhruv18999@gmail.com>
2023-10-10 22:46:19 -0500

    can submit this one

--- Assets/Assignment1/Scripts/HeroImageDetector.cs
+++ Assets/Assignment1/Scripts/HeroImageDetector.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d4112adca73e56db89a5d95462fc63d6fed70c07ea8fa44d6f0a90f7d98349d
-size 1654
+oid sha256:523cd41b28f73fb5ef4e13842bdf024dd321c7c127252ed848f617a616b2b322
+size 1709
