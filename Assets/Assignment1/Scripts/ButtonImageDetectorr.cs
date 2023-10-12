825099b99ed0f86a9d1ccdfb397ff095801c1e76
Dhruv <dhruv18999@gmail.com>
2023-10-12 14:14:54 -0500

    -- added tracker for action

---
+++ Assets/Assignment1/Scripts/ButtonImageDetectorr.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5da54c98448754fab0202b552929742347d37c3479125e54b5863864b801e7bc
+size 799
--- Assets/DataInClass/InClass/Scripts/AutoDestroyer.cs
+++ Assets/DataInClass/InClass/Scripts/AutoDestroyer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b456356a8e0661e91bb291b99f94471973ac6faf7601bbaaf28235aa67c31f84
-size 670
+oid sha256:6a54ad231a123181e57d9befed8a745956abf6c66a6f71b5c6770bebaa43b7dd
+size 899
